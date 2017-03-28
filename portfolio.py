f6c2a081c6718380140a84776c8382579cfc8a99
Wes Smiley <wes.smiley@phishme.com>
2017-03-28 15:42:53 -0400

    Updated footer

---
+++ portfolio.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
