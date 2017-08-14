dc464e73807d13eac6c89188a8eefe25538a9b3a
Wes Smiley <wesley.c.smiley@gmail.com>
2017-08-14 19:59:59 -0400

    Fixed quote issue with mail config

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '\xfb\x92$\x0f\xef\xb4(\xb5\xaf\x9f-9N\xf1\xc6h\x8f\x87\xc5L\xde\x9a=;'
 
-app.config['MAIL_SERVER'] = 'smtp.sendgrid.net
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
