0b0f04c9e76bfb46f1ff71f3e64cf673c986e708
Wes Smiley <wesley.c.smiley@gmail.com>
2017-08-14 19:54:43 -0400

    Testing mail

--- app.py
+++ app.py
@@ -6,20 +6,28 @@ from flask_mail import Mail, Message
 from forms import ContactForm
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '\xfb\x92$\x0f\xef\xb4(\xb5\xaf\x9f-9N\xf1\xc6h\x8f\x87\xc5L\xde\x9a=;'
 
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = 'admin@wsmiley.com'
 
-@app.route('/')
-@app.route('/index')
+mail = Mail(app)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = ContactForm()
+    if request.method == 'POST':
+        msg = Message('Testing')
+        msg.recipients = ['wesley.c.smiley@gmail.com']
+        msg.body = 'Testing 123'
+        mail.send(msg)
     return render_template('index.html', form=form)
-
-
-@app.route('/contact', methods=['POST'])
-def contact():
-    pass
-
 
 if __name__ == '__main__':
     app.run()
