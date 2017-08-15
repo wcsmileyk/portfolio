3e05c2ca88641f3facd67efa17148c153d59e9e2
Wes Smiley <wesley.c.smiley@gmail.com>
2017-08-14 21:33:47 -0400

    Added workout proj

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from forms import ContactForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '\xfb\x92$\x0f\xef\xb4(\xb5\xaf\x9f-9N\xf1\xc6h\x8f\x87\xc5L\xde\x9a=;'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
-app.config['MAIL_DEFAULT_SENDER'] = 'admin@wsmiley.com'
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 
 mail = Mail(app)
 
@@ -26,7 +26,7 @@ def index():
     if request.method == 'POST':
         if form.validate_on_submit():
             msg = Message('New Freelance Contact')
-            msg.recipients = ['wesley.c.smiley@gmail.com']
+            msg.recipients = [os.environ.get('MAIL_RECIPIENT')]
             contact_name = form.name.data
             contact_email = form.email.data
             contact_message = form.message.data
